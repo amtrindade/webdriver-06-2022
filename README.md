52dcfa611f57671ce7d0b1e58d640532f10a0234
amtrindade <amtrindade@gmail.com>
2022-06-15 06:56:51 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,13 @@
+# Projeto do curso Automação com WebDriver #
+
+## Projeto que está sendo desenvolvido no curso de Automação com WebDriver - Turma 06/22 ##
+
+### Dependências utilizadas: ###
+* Selenium WebDriver
+* TestNG
+* Apache POI
+
+### Pré-requisitos: ###
+* JDK >= 1.8
+* Google Chrome
+* Firefox
---
+++ src/test/java/com/webdriver/test/WebElementsTest.java
@@ -0,0 +1,53 @@
+package com.webdriver.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class WebElementsTest {
+	
+	WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/home/antonio/dev/drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("http://antoniotrindade.com.br/treinoautomacao/elementsweb.html");		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	public void testSendName() throws InterruptedException {
+		//Identifica o elemento 
+		WebElement textName = driver.findElement(By.name("txtbox1"));
+		
+		//ação de enviar texto
+		textName.sendKeys("Antônio");
+				
+		//Valido que o nome escrito é igual ao valor esperado
+		assertEquals("Deveria ter escrito o nome", 
+				"Antônio", textName.getAttribute("value"));
+	}
+	
+	@Test
+	public void testDisplay() {
+		WebElement textName = driver.findElement(By.name("txtbox1"));
+		WebElement textDisplay = driver.findElement(By.name("txtbox2"));
+				
+		assertTrue("Deveria estar habilitado", textName.isEnabled());
+	
+		assertFalse("Deveria estar desabilitado", textDisplay.isEnabled());
+			
+	}
+}
